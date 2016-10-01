8028667cd399403bbd35741427963ff1ffa37f21
lemossilva <guilhermelemos96@gmail.com>
2016-10-01 08:28:24 -0300

    Update plot3_2c.asm

--- Lab1/Part3/plot3_2c.asm
+++ Lab1/Part3/plot3_2c.asm
@@ -1,10 +1,10 @@
-.eqv VGA 0xFF000000		#ENDERE«O DA VGA
+.eqv VGA 0xFF000000		#ENDERE√áO DA VGA
 .eqv X $f0			#DEFINE X = $F20
 .eqv Y $f12			#DEFINE Y = $F21
 .eqv NUMx 320			#DEFINE NUMx = 320
 .eqv NUMy 239			#DEFINE NUMy = 240
 
-.data				#DECLARA VARI¡VEIS
+.data				#DECLARA VARI√ÅVEIS
 LINF:   .float -1.0
 LSUP:   .float 10.0
 NUMX:  .float 320.0
@@ -19,18 +19,18 @@ NUMY:  .float 240.0
 	l.s $f25, LSUP		#f25 = LSUP
 	l.s $f4, NUMX		#f4 = NUMX
 	l.s $f5, NUMY 		#f5 = NUMY
-	div.s $f26, $f25, $f4	#ResoluÁ„o de X
-	j LIMITS		#Calcula o m·ximo e o mÌnimo real da funÁ„o
-L2:	sub.s $f30, $f31, $f30	#ResoluÁ„o de Y
+	div.s $f26, $f25, $f4	#Resolu√ß√£o de X
+	j LIMITS		#Calcula o m√°ximo e o m√≠nimo real da fun√ß√£o
+L2:	sub.s $f30, $f31, $f30	#Resolu√ß√£o de Y
 	div.s $f30, $f30, $f5	
 	j PRINT			#PROCEDIMENTO PRINT
 	
 EXIT:	li $v0,10		#Encerra o programa
 	syscall
 	
-FUNCAO:	c.lt.s  X, $f18		#Separa NaN de n˙meros reais
+FUNCAO:	c.lt.s  X, $f18		#Separa NaN de n√∫meros reais
 	bc1t E1			
-	sqrt.s Y, X		#FunÁ„o da letra C
+	sqrt.s Y, X		#Fun√ß√£o da letra C
 	jr $ra
 E1:	mov.s Y, $f18		#Caso NaN, plota no zero, na cor verde
 	li $t5,0x38		
@@ -41,24 +41,24 @@ L1:	bne $t3, $t2,PLOT	#Se t3 != t2, executa plot
 	j EXIT
 	
 PLOT:	li $t5,0x7
-	la $s0, VGA		#s0 = endereÁo da VGA
+	la $s0, VGA		#s0 = endere√ßo da VGA
 	mtc1 $t2, X		#Passa t2 para CP1(FP)
 	cvt.s.w X, X		#Converte X para float
-	mul.s X, X, $f26	#Ajusta a resoluÁ„o de X
-	add.s X, X, $f24	#Ajusta X para comeÁar do LInf
-	jal FUNCAO		#Calcula a funÁ„o	
-	div.s Y, Y, $f30	#Ajusta a resoluÁ„o de Y
+	mul.s X, X, $f26	#Ajusta a resolu√ß√£o de X
+	add.s X, X, $f24	#Ajusta X para come√ßar do LInf
+	jal FUNCAO		#Calcula a fun√ß√£o	
+	div.s Y, Y, $f30	#Ajusta a resolu√ß√£o de Y
 	cvt.w.s Y, Y		#Converte Y de float para word
 	mfc1 $t4, Y		#Copia Y para t4
-	sub $t4, $t0, $t4	#Ajusta Y para comeÁar do (240,0), n„o do (0,0)
+	sub $t4, $t0, $t4	#Ajusta Y para come√ßar do (240,0), n√£o do (0,0)
 	mul $t4, $t4, $t1	#320*Y
 	add $t4, $t4, $t2	# +X
-	slti $t7, $t4, 0	#N„o pintar caso o byte calculado seja anterior ao inicio de 0xFF000000
+	slti $t7, $t4, 0	#N√£o pintar caso o byte calculado seja anterior ao inicio de 0xFF000000
 	beqz $t7, DRAW		
 	addi $t2, $t2, 1
 	j L1
 	
-DRAW:	add $t4, $s0, $t4	#Desenha o gr·fico
+DRAW:	add $t4, $s0, $t4	#Desenha o gr√°fico
 	sb $t5, 0($t4)
 	addi $t2, $t2, 1
 	j L1
@@ -67,7 +67,7 @@ DRAW:	add $t4, $s0, $t4	#Desenha o gr·fico
 LIMITS:	mtc1 $t2, X		#t2 varia de 0 a 319, move t2 para o registrador FP X
 	cvt.s.w X, X		#Converte para FP
 	add.s X, $f24, X	# X = X + Linf
-	mul.s X, $f26, X	#X = X * ResoluÁ„o de X
+	mul.s X, $f26, X	#X = X * Resolu√ß√£o de X
 	jal FUNCAO		#Calcula a funcao
 	mtc1 $zero, X
 	add.s $f30, $f18, Y	#Calcula o primeiro YMIN
@@ -76,20 +76,22 @@ LIMITS:	mtc1 $t2, X		#t2 varia de 0 a 319, move t2 para o registrador FP X
 LOOP:	mtc1 $t2, X		#t2 varia de 0 a 319, move t2 para o registrador FP X
 	cvt.s.w X, X		#Converte para FP
 	add.s X, $f24, X	# X = X + Linf
-	mul.s X, $f26, X	#X = X * ResoluÁ„o de X
-	jal FUNCAO		#Calcula a funÁ„o
+	mul.s X, $f26, X	#X = X * Resolu√ß√£o de X
+	jal FUNCAO		#Calcula a fun√ß√£o
 
 	c.le.s Y, $f30		#se Y<=YMIN 
 	bc1t YMIN
 	c.le.s $f31, Y		#se YMAX<= Y
 	bc1t YMAX
+	addi $t2, $t2, 1
+	j LOOP
 	
 YMIN:	mov.s $f30, Y		#Y = YMIN
-	beq $t3, $t2, L2	#CondiÁıes de Loop
+	beq $t3, $t2, L2	#Condi√ß√µes de Loop
 	addi $t2, $t2, 1
 	j LOOP
 
 YMAX:	mov.s $f31, Y		#Y = YMAX
-	beq $t3, $t2, L2	#CondiÁıes de loop
+	beq $t3, $t2, L2	#Condi√ß√µes de loop
 	addi $t2, $t2, 1
 	j LOOP	
