7e2a1500882b050e28d673ab0d483a075ce6b7ab
Abrantex <matheus.abrantesc@gmail.com>
2016-10-03 12:58:09 -0300

    Update plot3_2a.s

--- Lab1/Part3/plot3_2a.s
+++ Lab1/Part3/plot3_2a.s
@@ -1,93 +1,138 @@
-.eqv VGA 0xFF000000
-.eqv NUMX 320
-.eqv NUMY 240
+#
+#    IMPORTANTE : DEFINIR A FUNÇAO UTILIZADA E OS LIMITES SUPERIORES E INFERIORES ANTES DE USAR
+#
+#
+.eqv VGA 0xFF000000		#ENDEREÇO DA VGA
+.eqv X $f0			#DEFINE X = $F20
+.eqv Y $f12			#DEFINE Y = $F21
+.eqv ZERO $f18
+.eqv NUMx 320			#DEFINE NUMx = 320
+.eqv NUMy 129			#DEFINE NUMy = 240
 
-.data
-LINF:  	.float -1.0
-LSUP:	.float  1.0
-NX:	.float  320
-NY:	.float  240
-UM:	.float  1.0
-
+.data				#DECLARA VARIÁVEIS
+LINF:   .float -1		#Colocar aqui LINF
+LSUP:   .float 1		#Colocar aqui LSUP
+NUMX:  .float 320.0
+NUMY:  .float 240.0
+UM:	.float 1.0		#Declarar aqui números utilizados em sua função
+MEIOUM:	.float 1.5
 .text
-MAIN:	jal PLOT
-	li $v0,10
+	mtc1 $zero, $f18	#Define um zero em float
+	la $t1, NUMx		#t1 = 320
+	la $t0, NUMy		#t0 = 129
+	l.s $f11, UM		#f11 = 1.0
+	l.s $f27, MEIOUM	#f27 = 1.5
+	addi $t3, $t1, 1	
+	l.s $f24, LINF		#f24 = LINF
+	l.s $f25, LSUP		#f25 = LSUP
+	l.s $f4, NUMX		#f4 = NUMX
+	l.s $f5, NUMY 		#f5 = NUMY
+	sub.s $f26, $f25, $f24	#Resolução de X
+	div.s $f26, $f26, $f4	#Resolução de X
+	j LIMITS		#Calcula o máximo e o mínimo real da função
+L2:	c.le.s $f18, $f30	#Verifica se Ymin é menor que zero
+	bc1t Y0 		
+	sub.s $f30, $f31, $f30	#Resolução de Y
+	div.s $f30, $f30, $f5	#Resolução de Y
+	j PRINT			#PROCEDIMENTO PRINT
+	
+Y0:	mul $t0, $t0, $zero	#Reajusta o offset para Y começar em 239, 0
+	addi $t0, $t0, 239
+	sub.s $f30, $f31, $f30	#Resolução de Y
+	div.s $f30, $f30, $f5	#Resolução de Y
+	j PRINT			#PROCEDIMENTO PRINT
+	
+EXIT:	li $v0,10		#Encerra o programa
 	syscall
-
-FUNCAO:	# letra b)
-	l.s $f1,UM
-	mul.s $f12,$f0,$f0
-	add.s $f12,$f12,$f1
-	jr $ra	
-			
-PLOT:	
-	ori $t5,$zero,320
- 	
-	
-	#plot
-	la $at, LINF
-	lwc1 $f1, 0($at)	# -1.0 LINF
-	
-	
-	la $at, LSUP
-	lwc1 $f2, 0($at)	# 1.0
-	
-	
-	la $at,NX
-	lwc1 $f4, 0($at)	#320 pixels
-	
-	sub.s $f3,$f2,$f1	#  f3 = fsup - flinf
-	div.s $f3,$f3,$f4	# f3 = x_resol
-	
-				
-	lui $at,0x3f40		#
-	mtc1 $at,$f4		# 0.75 = 240/320
-	
-	
-	div.s $f4,$f3,$f4	# f4 = y_resol
-	
-	
-	andi $s2,$s2, 0		#força os bits de s2 a ser 0 #primeiro pixel de x
-	 
-	add.s $f5,$f0,$f0 	# f5 = 0+0  #primeiro pixel x 
-	
-	
-	
-loop:				#realiza essa linha 
-	mul.s $f6,$f5,$f3 	# f6 = f5 * x_resol
-	add.s $f6,$f6,$f1	# f6 = f6 + fLINF 
-	sub.s $f6,$f0,$f6	# mesma coisa que f6 = -f6  func
-	# jal func
-	sub.s $f6,$f6,$f1	# f6 = f6 - fLINF
-	div.s $f6,$f6,$f4 	# f6 = f6/y_resol ou  f6 = f6*0.75
-	
-	cvt.w.s $f7, $f6  	#converte float para int
-	mfc1 $t4,$f7
-	
-	
-	nor $t4,$t4,$t4         #faz t4= t4' para poder somar com um e obter -t4					<<<<
-	addi $t4,$t4,241	#toma 240 para arrumar o numero de pixels e 1 para negativar t4                     	<<<<<
-	
-	slti $at,$t4,240       # veriica se o ultrapassa o numero de pixels   t4<240 ? 1:0
-	beq $at,$zero, pula    # ultrapassa numero de pixels 
-	slti $at,$t4,0         #t4 <0 ? 1:0
-	bne $at,$zero,pula     #ultrapassa
-
-	#pinta 	 	 	
-	lui $t1, 0XFF00		#prepara para 	
-	mul  $t4, $t4, $t5	# y*320	
-	add  $t4, $s2, $t4
-	add  $t1, $t4, $t1	
-	addi $t0, $zero,0	
-	sb $t0, 0($t1)		
-	
-	
-pula:	addi $s2,$s2,1		#  s2++
-	mtc1 $s2,$f5
-	cvt.s.w $f5,$f5
-	#add.s $f5,$f5,$f2     	# f5 = f5 + 1 (pois f2 = LSUP) 		
-	slti $t3,$s2,320		#  s2< 10 ? 1:0
-	bne  $t3,$zero,loop	#  t3 !=  0 ? loop : pc+4 
 	
 
+FUNCAO:	#INSERIR AQUI A FUNÇÃO. CASO EXISTAM NÚMEROS IMAGINÁRIOS NA IMAGEM DA FUNÇÃO
+	#PARA O DOMÍNIO SELECIONADO, REDIRECIONAR CASOS PARA O LABEL E1
 	
+	mtc1 $zero,$f10
+	sub.s $f12,$f10,$f0
+	
+	#RECEBE X EM $F0 E RETORNA Y EM $F12
+	jr $ra			#Retorna pra PC+4
+E1:	li $t5, 0xFF		#Para NaN, plotar em branco
+	jr $ra			#Retorna pra PC+4 (caso X seja 1.5)
+	
+PRINT:	mov.s X, $f24		#Reseta X
+	mul $t2, $t2, 0
+	c.eq.s X, $f25		#Condições de Loop
+	bc1f PLOT		#Se X != LSUP, executa plot	
+	j EXIT
+	
+PLOT:	la $s0, VGA		#s0 = endereço da VGA
+L1:	li $t5,0x7		#Cor vermelha
+	jal FUNCAO		#Calcula a função
+	div.s Y, Y, $f30	#Ajusta a resolução de Y
+	c.lt.s Y, $f18		#Se Y<0, vá para L3
+	bc1t L3
+	cvt.w.s Y, Y		#Converte Y de float para word
+	mfc1 $t4, Y		#Copia Y para t4
+	sub $t4, $t0, $t4	#Ajusta Y para começar do (129,0) ou (239,0), não do (0,0)
+	mul $t4, $t4, $t1	#320*Y
+	add $t4, $t4, $t2	# +X
+	slti $t7, $t4, 0	#Não pintar caso o byte calculado seja anterior ao inicio de 0xFF000000
+	beqz $t7, DRAW		
+	c.le.s $f25, X		#Condições de Loop
+	bc1t EXIT
+	addi $t2, $t2, 1	#t2 = t2 + 1
+	add.s X, X, $f26	#X = X + Resolução em X
+	j L1
+
+#O meio da tela é o eixo X, ou seja Y =0. Caso Y seja menor que zero, o ajuste do offset é (129 + módulo de Y*Resolução_Y)(L3)
+#Caso Y seja maior que zero, o ajuste de offset é 129 - (Y*resolução de Y)
+
+L3:	abs.s Y, Y		#Tira o módulo de Y
+	cvt.w.s Y, Y		#Converte Y de float para word
+	mfc1 $t4, Y		#Copia Y para t4
+	add $t4, $t0, $t4	#Ajusta Y para começar do (129,0) ou (239,0), não do (0,0)
+	mul $t4, $t4, $t1	#320*Y
+	add $t4, $t4, $t2	# +X
+	slti $t7, $t4, 0	#Não pintar caso o byte calculado seja anterior ao inicio de 0xFF000000
+	beqz $t7, DRAW
+	c.le.s $f25, X		#Condições de Loop
+	bc1t EXIT
+	addi $t2, $t2, 1	#t2 = t2 + 1
+	add.s X, X, $f26	#X = X + Resolução em X
+	j L1
+	
+DRAW:	add $t4, $s0, $t4	#Desenha o gráfico
+	sb $t5, 0($t4)
+	c.le.s $f25, X		#Condições de Loop
+	bc1t EXIT
+	addi $t2, $t2, 1	#t2 = t2 + 1
+	add.s X, X, $f26	#X = X + Resolução em X
+	j L1
+
+	
+LIMITS:	add.s X, $f24, X	# X = X + Linf
+	jal FUNCAO		#Calcula a funcao
+	mtc1 $zero, X		#Certifica que X é 0
+	add.s $f30, ZERO, Y	#Calcula o primeiro YMIN
+	add.s $f31, ZERO, Y	#Calcula o primeiro YMAX
+	
+	add.s X, $f24, X	# X = X + Linf
+LOOP:	jal FUNCAO		#Calcula a função
+	c.le.s Y, $f30		#se Y<=YMIN 
+	bc1t YMIN
+	c.le.s $f31, Y		#se YMAX<= Y
+	bc1t YMAX
+	c.le.s $f25, X		#Condições de Loop
+	bc1t L2
+	add.s X, X, $f26
+	j LOOP
+	
+YMIN:	mov.s $f30, Y		#Y = YMIN
+	c.le.s $f25, X		#Condições de Loop
+	bc1t L2
+	add.s X, X, $f26
+	j LOOP
+
+YMAX:	mov.s $f31, Y		#Y = YMAX
+	c.le.s $f25, X		#Condições de Loop
+	bc1t L2
+	add.s X, X, $f26
+	j LOOP	
